f0747b1557b27fab57b4c3e41dde8b66e5e71a65
Shaun Chiang <redbeansodapop@hotmail.com>
2018-02-17 02:26:05 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import { ScrollView, Text, StyleSheet, ToastAndroid } from 'react-native';
+import HTMLView from 'react-native-htmlview';
+
+var REQUEST_URL = 'https://www.zionserangoon.org.sg/wp-json/wp/v2/pages';
+
+export default class DetailsScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: { title: "Loading Title..", content: "Loading Content.." }
+    }
+  }
+  render() {
+    htmlToShow = this.state.data.content
+    return (<ScrollView style={{ flex: 1 }}>
+      <Text>{this.state.data.title}</Text>
+      <HTMLView value={htmlToShow} stylesheet={styles}></HTMLView>
+    </ScrollView>)
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch(REQUEST_URL).then((response) => response.json())
+      .then((responseData) => {
+        //TODO: This shouldn't be hardcoded.
+        this.setState({ title: responseData[2].title.rendered, content: responseData[2].content.rendered }
+        );
+      })
+      .done();
+  }
+}
+const styles = StyleSheet.create({
+  a: {
+    fontWeight: '300',
+    color: '#83d87d'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+
+  }
+})
